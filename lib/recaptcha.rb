e4c2388c347368ef9cdc5a87b4f31906af9baed9
Andrew Bloom <andrew@andrewkbloom.com>
2008-08-07 15:04:02 -0500

    made private_key an options for verify_recaptcha

--- lib/recaptcha.rb
+++ lib/recaptcha.rb
@@ -3,22 +3,27 @@ module Ambethia
   module ReCaptcha
     RECAPTCHA_API_SERVER        = 'http://api.recaptcha.net';
     RECAPTCHA_API_SECURE_SERVER = 'https://api-secure.recaptcha.net';
-    RECAPTCHA_VERIFY_SERVER     = 'api-verify.recaptcha.net';
+    RECAPTCHA_VERIFY_SERVER     = 'http://api-verify.recaptcha.net/verify';
 
     SKIP_VERIFY_ENV = ['test']
+
+    mattr_accessor :public_key, :private_key
 
     module Helper 
       # Your public API can be specified in the +options+ hash or preferably the environment
       # variable +RECAPTCHA_PUBLIC_KEY+.
       def recaptcha_tags(options = {})
         # Default options
-        key   = options[:public_key] ||= ENV['RECAPTCHA_PUBLIC_KEY']
-        error = options[:error] ||= session[:recaptcha_error]
-        uri   = options[:ssl] ? RECAPTCHA_API_SECURE_SERVER : RECAPTCHA_API_SERVER
-        xhtml = Builder::XmlMarkup.new :target => out=(''), :indent => 2 # Because I can.
+        key   = (options[:public_key] || Ambethia::Recaptcha.public_key)
+        raise ReCaptchaError, "No public key specified." unless key
+        error = (options[:error] || session[:recaptcha_error])
+        uri   = (options[:ssl] ? RECAPTCHA_API_SECURE_SERVER : RECAPTCHA_API_SERVER)
+        xhtml = Builder::XmlMarkup.new(:target => out=(''), :indent => 2) # Because I can.
+
         if options[:display] 
           xhtml.script(:type => "text/javascript"){ xhtml.text! "var RecaptchaOptions = #{options[:display].to_json};\n"}
         end
+        
         if options[:ajax]
          xhtml.div(:id => 'dynamic_recaptcha') {}
          xhtml.script(:type => "text/javascript", :src => "#{uri}/js/recaptcha_ajax.js") {}
@@ -39,24 +44,29 @@ module Ambethia
             end
           end
         end
-        raise ReCaptchaError, "No public key specified." unless key
+        
         return out
       end # recaptcha_tags
     end # Helpers
     
     module Controller
       # Your private API key must be specified in the environment variable +RECAPTCHA_PRIVATE_KEY+
-      def verify_recaptcha(model = nil)
+      def verify_recaptcha(model = nil, options = {})
         return true if SKIP_VERIFY_ENV.include? ENV['RAILS_ENV']
-        raise ReCaptchaError, "No private key specified." unless ENV['RECAPTCHA_PRIVATE_KEY']
+        
+        key = (options[:private_key] || Ambethia::Recaptcha.private_key)
+        raise ReCaptchaError, "No private key specified." unless key
+        
         begin
-          recaptcha = Net::HTTP.post_form URI.parse("http://#{RECAPTCHA_VERIFY_SERVER}/verify"), {
-            :privatekey => ENV['RECAPTCHA_PRIVATE_KEY'],
+          logger.info "Calling reCAPTCHA: #{RECAPTCHA_VERIFY_SERVER}"
+          recaptcha = Net::HTTP.post_form(URI.parse(RECAPTCHA_VERIFY_SERVER), {
+            :privatekey => key,
             :remoteip   => request.remote_ip,
             :challenge  => params[:recaptcha_challenge_field],
             :response   => params[:recaptcha_response_field]
-          }
+          })
           answer, error = recaptcha.body.split.map { |s| s.chomp }
+          logger.info "reCAPTCHA returned: #{answer.inspect}, #{error.inspect}"
           unless answer == 'true'
             session[:recaptcha_error] = error
             model.valid? if model
